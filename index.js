02e1a9e9bce3f06694a872f0953c60a63c95ed6c
Pablo Cruz <pcruzp1400@gmail.com>
2024-06-09 03:50:04 -0600

    Add endpoints

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const server = require("./api/server");
+const server = require("./src/server");
 const db = require("./src/lib/db");
 const PORT = process.env.PORT || 8080;
 
